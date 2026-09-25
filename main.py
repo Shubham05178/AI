d41e926eee8658fdde4334cc457bb1a49195de62
Shubham Nagaria <shubhamnagaria@Shubhams-MacBook-Air.local>
2026-09-25 14:01:46 +0530

    Added

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastmcp import FastMCP
+import random
+mcp=FastMCP(name="Demo Server")
+
+@mcp.tool
+def roll_dice(n_dice:int =1)-> list[int]:
+    "Roll n_dice 6-sided dice and return the result"
+    return [random.randint(1,6) for _ in range(n_dice)]
+@mcp.tool
+def add_number(a:float,b:float)->float:
+    "Add two numbers"
+    return a+b
+if __name__=="main":
+    mcp.run()
